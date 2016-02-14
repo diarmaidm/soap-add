3a9c5518e06d43eee9e7d421bd5271c1de5c9ec9
diarmaidm <diarmaidm@users.noreply.github.com>
2016-02-14 20:34:53 +0000

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var soap = require('soap');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'soap add application' });
+});
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  var a = req.body.a, b = req.body.b;
+  var url = 'http://free-web-services.com/add.wsdl';
+  var args = "<tns:addRequest><a>" + a + "</a><b>" + b + "</b></tns:addRequest>";
+
+  soap.createClient(url, function(err, client) {
+    client.add(args, function(err, result) {
+      res.render('index', { a: a, b: b, result: result.sum });
+    });
+  });
+});
+
+module.exports = router;
---
+++ test/integration/index_test.js
@@ -0,0 +1,53 @@
+var request = require('supertest');
+var app = require('../../app');
+var expect = require('chai').expect;
+
+describe('Home Page', function () {
+  it('has a header describing what it does', function (done) {
+    request(app).get('/')
+      .expect(/This application uses a soap service to add 2 numbers/, done);
+  });
+
+  it('handles a GET request', function (done) {
+    request(app).get('/')
+      .expect(200, done);
+  });
+
+  describe('contains a form', function () {
+    it('contains fields to allow entry of 2 numbers', function (done) {
+      request(app).get('/')
+        .expect(/name="a"/)
+        .expect(/name="b"/, done);
+    });
+
+    it('contains a button to submit the request', function (done) {
+      request(app).get('/')
+        .expect(/action="\/"/)
+        .expect(/type="submit"/)
+        .expect(/method="POST"/, done);
+    });
+ 
+    it('adds 2 and 4 and shows the sum as 6 when submit clicked', function (done) {
+      var a = 2, b = 4;
+      request(app).post('/')
+        .send({a: a, b: b})
+        .end(function (req, res) {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.contain('The sum of ' +a+ ' and ' +b+ ' is: ' + (a+b) );
+          done();
+        });
+    });
+
+    it('adds 4 and 4 and shows the sum as 8 when submit clicked', function (done) {
+      var a = 4, b = 4;
+      request(app).post('/')
+        .send({a: a, b: b})
+        .end(function (req, res) {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.contain('The sum of ' +a+ ' and ' +b+ ' is: ' + (a+b) );
+          done();
+        });
+    });
+  });
+});
+
