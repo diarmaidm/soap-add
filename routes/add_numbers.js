c6e830495f86dcdb2492a7729dfbfe1a51b9e34c
Diarmaid <diarmaidm@users.noreply.github.com>
2016-03-23 00:37:07 +0000

    handle when the values are numbers that are strings

--- routes/add_numbers.js
+++ routes/add_numbers.js
@@ -1,5 +1,5 @@
 function addNumbers(a, b) {
-  return a+b;
+  return Number(a)+Number(b);
 }
 
 module.exports = addNumbers;
--- test/integration/index2_test.js
+++ test/integration/index2_test.js
@@ -41,4 +41,17 @@ describe('new add page', function () {
         done();
       });
   });
+
+  it('adds strings "3" and "4" and shows the sum as 7', function (done) {
+    var a = "3", b = "4";
+    request(app).post('/index2')
+      .send({a: a, b: b})
+      .end(function (req, res) {
+        expect(res.status).to.equal(200);
+        expect(res.text).to.contain( 7 ); // fails - concat not add
+        //expect(res.text).to.contain( (a+b) ); bad compare
+        done();
+      });
+  });
+
 });
