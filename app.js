ab748dcb490e6c34ede68561133ce671e14c2b89
diarmaidm <diarmaidm@users.noreply.github.com>
2016-05-06 00:01:08 +0100

    added favicon

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon-32x32.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
